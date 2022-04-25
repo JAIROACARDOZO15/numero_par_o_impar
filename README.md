c32b3bbbdbe3f3cdf0e3b6e413fbf17855bccbce
JAIROACARDOZO15 <jairo2224637@correo.uis.edu.co>
2022-04-25 09:22:03 -0500

    DESARROLLO TALLER

---
+++ README.md
@@ -0,0 +1,6 @@
+# ejercicio No. 6 #
+
+## programa para saber si un numero es par o impar
+
+### Diagrama de flujo
+![digramadeflujo](numeropar.png) 
---
+++ numeroparoimpar.py
@@ -0,0 +1,13 @@
+"""Programa para saber si un numero es par o impar"""
+
+print("---------------------------------------")
+print("----------Numero par o impar ----------")
+print("---------------------------------------")
+
+x = int(input(" ingrese el numero: "))
+
+if x %2 == 0:
+    z = print(" EL NUMERO ES PAR")
+
+else:
+    z = print("EL NUMERO ES IMPAR")
